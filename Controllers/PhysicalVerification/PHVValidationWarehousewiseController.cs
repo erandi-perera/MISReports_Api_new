4244c71bd860ff993fcb05e2aede55af04b8f577
RashmiFernando <it22177414@my.sliit.lk>
2026-01-19 13:52:15 +0530

    Created backend for the PHVValidationWarehousewise

---
+++ Controllers/PhysicalVerification/PHVValidationWarehousewiseController.cs
@@ -0,0 +1,42 @@
+﻿using MISReports_Api.DAL.PhysicalVerification;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MISReports_Api.Controllers
+{
+    [RoutePrefix("api/phv-validation-warehousewise")]
+    public class PHVValidationWarehousewiseController : ApiController
+    {
+        private readonly PHVValidationWarehousewiseRepository _repository;
+
+        public PHVValidationWarehousewiseController()
+        {
+            _repository = new PHVValidationWarehousewiseRepository();
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public async Task<IHttpActionResult> GetWarehousewiseValidation(
+            string deptId,
+            string warehouseCode,
+            int repYear,   
+            int repMonth) 
+        {
+            try
+            {
+                var data = await _repository.GetWarehousewiseValidationAsync(
+                    deptId,
+                    warehouseCode,
+                    repYear,
+                    repMonth);
+
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
