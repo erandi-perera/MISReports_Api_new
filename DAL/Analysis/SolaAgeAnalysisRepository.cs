a33149c9f69ec6efea519b088a89195387def7b4
Sudesi7 <155457820+Sudesi7@users.noreply.github.com>
2026-02-13 14:56:50 +0530

    create Sola Age Analysis report

---
+++ DAL/Analysis/SolaAgeAnalysisRepository.cs
@@ -0,0 +1,15 @@
+﻿using MISReports_Api.Models;
+
+namespace MISReports_Api.DAL.Analysis
+{
+    public class SolarAgeAnalysisRepository
+    {
+        private readonly SolarAgeAnalysisDao _dao =
+            new SolarAgeAnalysisDao();
+
+        public SolarAgeSummaryModel GetSummary(string areaCode, int billCycle)
+        {
+            return _dao.GetSummary(areaCode, billCycle);
+        }
+    }
+}
---
+++ DAL/Analysis/SolarAgeCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using MISReports_Api.Models.Analysis;
+using System.Collections.Generic;
+
+namespace MISReports_Api.DAL.Analysis
+{
+    public class SolarAgeCategoryRepository
+    {
+        private readonly SolarAgeCategoryDao _dao =
+            new SolarAgeCategoryDao();
+
+        public List<SolarAgeCategoryDetailModel> GetByCategory(
+            string areaCode,
+            int billCycle,
+            string category)
+        {
+            return _dao.GetByCategory(areaCode, billCycle, category);
+        }
+    }
+}
