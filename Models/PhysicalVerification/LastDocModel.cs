f60afb29a0b55a6add45da8e8f969d8e7d42ae40
RashmiFernando <it22177414@my.sliit.lk>
2026-03-04 14:02:24 +0530

    Created backend files
    -Created backend for PHVDamageBOS
    -Created backend for PHVObsoleteIdleBOS
    -Created backend for LastDocument

---
+++ Models/PhysicalVerification/LastDocModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MISReports_Api.Models
+{
+    public class LastDocModel
+    {
+        public string DocPrefix { get; set; }
+        public string MaxDocNo { get; set; }
+        public DateTime? MaxTrxDate { get; set; }
+        public string CostCenterName { get; set; }
+    }
+}
